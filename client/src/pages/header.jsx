7c82c8d117760e94655b2e11a95be44f531e06c3
Rohit-Prakash-27 <rohitprakashh27@gmail.com>
2025-10-24 22:07:53 +0530

    Updated header tagline

--- client/src/pages/header.jsx
+++ client/src/pages/header.jsx
@@ -3,8 +3,8 @@ function Header(){
         <>
         {/* Header */}
       <div className="row p-3 bg-dark text-light text-center p">
-        <h1 className="todo-title">Todo-Pro</h1>
-        <p className="todo-subtitle">Your Productivity Partner</p>
+        <h1 className="todo-title">Task-Pilot</h1>
+        <p className="todo-subtitle">Where Planning Meets Progress</p>
       </div>
         </>
     )  
